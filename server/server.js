5f233b9196c20e62d38b79ebff2cafc9bb1e0814
bryanmojica <bryan@trestian.com>
2016-09-08 13:32:59 -0700

    re-implement db, reinstate db references

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-// var db = require('../db');
+var db = require('./db');
 var drawingRouter = require('./resources/drawingRouter');
 var path = require('path');
 
