5ce0209699de811b7f53f4e26a07c8eec477bb92
bryanmojica <bryan@trestian.com>
2016-09-08 16:32:01 -0700

    implement voteController; DB iterative updates

--- server/resources/drawingRouter.js
+++ server/resources/drawingRouter.js
@@ -5,4 +5,8 @@ drawingRouter.route('/:roundId')
   .get(drawingController.retrieveRoundsDrawings)
   .post(drawingController.addDrawing);
 
+drawingRouter.route('/vote/:roundId')
+  .get(voteController.retrieveRoundsVotes)
+  .post(voteController.addVote);
+
 module.exports = drawingRouter;
---
+++ server/resources/voteController.js
@@ -0,0 +1,11 @@
+var Drawing = require('./Drawing');
+
+exports.addVote = function (req, res) {
+  var vote = req.params; // keep eye on
+  // update drawing vote count - using image.Id as ref
+}
+
+exports.retrieveRoundVotes = function (req, res) {
+  var vote = req.params; // keep eye on
+  // update drawing vote count - using image.Id as ref
+}
