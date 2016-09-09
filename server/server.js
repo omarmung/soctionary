8fca98b1dca5b226e06a37be4ba8e72aa2d7d862
Drew Kosta <drewkosta@gmail.com>
2016-09-08 20:31:03 -0700

    wire up db and gen backend cleanup

--- server/server.js
+++ server/server.js
@@ -1,40 +1,29 @@
-// requires
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var db = require('./db');
-var drawingRouter = require('./resources/drawingRouter');
+var drawingController = require('./resources/drawingController');
 var path = require('path');
 
-// static routes
 app.use(express.static('public'));
 app.use('/static', express.static(__dirname + '/../public'));
 app.use('/static', express.static(__dirname + '/../public/node_modules/fabric/dist'));
-app.use('/api/drawings', drawingRouter);
 
 var animals = [];
-
 var clients = {};
+var rounds = 0;
+var queried = false;
+var images;
 
 io.on('connection', function(socket) {
-  // do something on connect
-  //console.log('NEW SOCKET CONNECTION!', socket);
   
   socket.on('name', function (name) {
     socket.name = name;
     clients[name] = 0;
     io.emit('readyView');
   });
-  
-// io.sockets.on('connect', function(client) {
-//     clients[client.name] = 0; 
-//     client.on('disconnect', function() {
-//       delete clients.client;
-//     });
-    
-// });
 
   socket.on('ready', function () {
     io.sockets.emit('countdown', animals[Math.floor(Math.random() * animals.length)]);
@@ -43,55 +32,53 @@ io.on('connection', function(socket) {
       setTimeout(function () {
         io.emit('end');
       }, 5000);
-    }, 3000);
+    }, 4000);
   });
   
   socket.on('image', function (data) {
-    // convert to image and save in db
-      /* when all imgs in db */
-      io.emit('vote', images);
-      setTimeout(function () {
-        var winner = {
-          votes: 0,
-          name: ''        
-        }
-        for (var name in clients) {
-          if (clients[name] > winner.votes) {
-            winner.name = name;
-            winner.votes = clients[name];
-          }
-        }
-        io.emit('results', winner);
-      }, Math.max(10000, clients.length * 2000));
+    
+    drawingController.addDrawing({
+      playerName: socket.name,
+      roundId: rounds,
+      vectorDrawing: data
+    });
+    
+    setTimeout(function () {
+      if (!queried) {
+        drawingController.retrieveRoundsDrawings(rounds, function (images) {
+          io.sockets.emit('vote', images);
+        });
+        queried = true;
+      }
+    }, 4000);
+      
   });
-  
+      
   socket.on('vote', function (name) {
     clients[name]++;
+    
+    setTimeout(function () {
+      for (var i = 0; i < images.length; i++) {
+        images[i].votes = clients[images[i][name]];
+      }
+      io.emit('results', images);
+    }, Math.max(10000, clients.length * 2000));
+    
   });
-  // socket.on('draw', function (data) {
-  //   io.emit('draw', data);
-  //   console.log(data);
-  // });
-  // console.log('ON SOCKET CONNECTION HERE WE HAVE', socket);
 
   socket.on('again', function () {
+    rounds++;
+    queried = false;
     io.emit('readyView');
   });
 
   socket.on('disconnect', function (something) {
     console.log('A SOCKET DISCONNECTED!');
-    delete clients[socket.name];   
+    delete clients[socket.name];
   });
 
 });
 
-//routes
-app.get('/test', function(req, res) {
-  console.log('endpoint responding...');
-  res.end('Test');
-});
-
-// start server
 http.listen(8080, function(data) {
   console.log('server starting...');
 });
