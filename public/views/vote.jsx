c0745ae5b7c2164b3fbedbc8fa81d433459a8da3
Dustin Kimmel <dkimmel@gmail.com>
2016-09-12 11:06:22 -0700

    Add dynamic voting view countdown timer and loading screen div/text

--- public/views/vote.jsx
+++ public/views/vote.jsx
@@ -13,9 +13,33 @@ export default class Vote extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			renderInfo: []
+			renderInfo: [],
+			remainingTime: 10
 
 		}
+	}
+
+	componentDidMount() {
+  document.getElementsByClassName('votingCountdown')[0].style.display = 'none';
+  document.getElementsByClassName('waitTime')[0].style.display = 'inline';
+
+	// componentWillUnmount() {
+	// 	clearInterval(this.timer);
+	}
+
+  tick() {
+  	this.setState({remainingTime: this.state.remainingTime - 1});
+  	console.log('tick: ' + this.state.remainingTime);
+    if (this.state.remainingTime <= 1) {
+    	clearInterval(this.timer);
+    	this.setState({remainingTime: '!'});
+    	setTimeout(this.hideCountDown.bind(this), 1000);
+      ;
+    }
+  }
+
+	hideCountDown() {
+		document.getElementsByClassName('votingCountdown')[0].style.display = 'none';
 	}
 
 	componentWillMount() {
@@ -23,12 +47,12 @@ export default class Vote extends React.Component {
 		socket.on('vote', function (data) {
 			//time for countdown
 			var time = data.time;
+			this.setState({remainingTime: time});
 			var canvas = new fabric.Canvas('test')
 			//var images = [];
 			data.images.forEach( function(blob) {
 				//images.push(blob.vectorDrawing);
 			  	canvas.loadFromJSON( blob.vectorDrawing, function() {
-
 
 			  		var image = canvas.toDataURL({
 							format: 'image/png',
@@ -52,9 +76,12 @@ export default class Vote extends React.Component {
 		  // redirect to voting view
 		  // images is an array of JSON.stringify(canvas) objects to vote on
 		  //this.renderDrawings(images);
-
-
-
+  
+    	console.log('vote countdown started...');
+    	this.timer = setInterval(this.tick.bind(this), 1000);
+    	document.getElementsByClassName('votingCountdown')[0].style.display = 'inline';
+  	  document.getElementsByClassName('waitTime')[0].style.display = 'none';
+      
 		}.bind(this));
 
 		socket.on('countVotes', function()  {
@@ -63,11 +90,7 @@ export default class Vote extends React.Component {
 			socket.removeListener('countVotes')
 			window.location.href = '#/result' 
 		}.bind(this))
-
-
 	} 
-
-
 
 	getVotedName() {
 
@@ -94,6 +117,11 @@ export default class Vote extends React.Component {
 		//Need to decide if we use one big canvas, or just render images of all the drawings
 		return (
 			<div id="vote">
+			  <div className='waitTime'>Loading...</div>
+			  <div className='votingCountdown valign'>
+			  	<div className="votePrompt">Pick your favorite!</div>
+			  	<div className="voteCountdown"> {this.state.remainingTime} </div>
+			  </div>
 				{this.state.renderInfo.map((data) => 
 					<Select id={data.id} name = {data.name} voting={this.voting.bind(this)} image={data.image}/>
 				)}
